b4427d1b6ba519373b073eccda3e2b206730c395
Yukako Shimizu <ykksmz1043@gmail.com>
2022-09-19 21:10:46 +0900

    add main

--- show.py
+++ show.py
@@ -1,6 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import random
 
-with open("./hyakunin.txt", encoding="utf-8") as f:
-    wakas = [s.strip() for s in f.readlines()]
+def main():
 
-print(wakas[random.randrange(len(wakas))])
+    with open("./hyakunin.txt", encoding="utf-8") as f:
+        wakas = [s.strip() for s in f.readlines()]
+        print(wakas[random.randrange(len(wakas))])
+
+if __name__ == '__main__':
+    main()
