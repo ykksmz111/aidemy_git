a5d71df611998fa80bfee548635e7de67f0e73e1
Yukako Shimizu <ykksmz1043@gmail.com>
2022-09-19 21:15:53 +0900

    add today

--- show.py
+++ show.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import random
 
 with open("./hyakunin.txt", encoding="utf-8") as f:
     wakas = [s.strip() for s in f.readlines()]
 
-print(wakas[random.randrange(len(wakas))])
+print("今日の一句" + wakas[random.randrange(len(wakas))])
